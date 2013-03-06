7001097375dce4ef658d5eb75a33e256e575506f
Dave Brooks <dave@bcs.co.nz>
2013-03-06 13:02:38 +1300

    Get 'strm2bsml' working.

--- strm2bsml/strm2bsml.py
+++ strm2bsml/strm2bsml.py
@@ -4,6 +4,8 @@ import numpy as np
 from biosignalml.client import Repository
 from biosignalml.data import UniformTimeSeries
 import biosignalml.units as units
+import biosignalml.rdf as rdf
+
 
 VERSION = '0.1'
 
@@ -15,7 +17,7 @@ if __name__ == '__main__':
 
   import os, sys
 
-  USAGE = 'Usage: %s [-r REPOSITORY] RECORDING RATE ([-p POS] [-u units] SIGNAL)+' % sys.argv[0]
+  USAGE = 'Usage: %s [options] RECORDING_URI RATE ([-p POS] [-u UNITS] SIGNAL_ID)+' % sys.argv[0]
 
   ## [-c CHANNELS] # Or determine from first line of input ( = len(l.split()) - 1)
 
@@ -24,26 +26,26 @@ if __name__ == '__main__':
     if msg: sys.stderr.write('%s\n' % msg)
     sys.exit(USAGE)
 
-
   def parse_args(args):
   #--------------------
     if len(args) == 1 and args[0] == '-h':
       print USAGE
       print """ """
       sys.exit(0)
-    if len(args) < 3: error_exit()
-    n = 0
-    repo = None
-    if args[0] == '-r':
-      repo = args[1]
-      if not repo.startswith('http://'):
-        error_exit('Invalid repository URI')
-      n = 2
-    rec_uri = args[n]
-    if not rec_uri.startswith('http://'):
+    if len(args) < 4: error_exit()
+
+    parameters = { }
+
+    n = 1
+    if args[n] == '-d':
+      n += 1
+      parameters['delimiter'] = args
+      n += 1
+    if not args[n].startswith('http://'):
       error_exit('Invalid recording URI')
+    parameters['recording'] = args[n]
     n += 1
-    try: rate = float(args[n])
+    try: parameters['rate'] = float(args[n])
     except ValueError: error_exit('Invalid rate')
     n += 1
     signals = [ ]
@@ -69,43 +71,52 @@ if __name__ == '__main__':
         n += 1
     except IndexError:
       error_exit('Missing signal argument')
-    return(repo, rec_uri, rate, signals)
-
-  rep_uri, rec_uri, rate, sig_details = parse_args(sys.argv[1:])
-
-  if rep_uri is None:
-    p = urlparse.urlparse(rec_uri)
-    rep_uri = p.scheme + '://' + p.netloc
+    parameters['signals'] = signals
+    return parameters
 
 
+  args = parse_args(sys.argv)
 
+  rec_uri = args['recording']
+  rate = args['rate']
+  signals = args['signals']
+
+  p = urlparse.urlparse(rec_uri)
+  rep_uri = p.scheme + '://' + p.netloc
 
 ##  print rep_uri, rec_uri, rate, sig_details  ######
 
   repo = Repository(rep_uri)
   rec = repo.new_recording(rec_uri)  ##, description=, )
   signals = [ ]
-  for n, s in enumerate(sig_details):
-    if s[2].startswith('http://'): (uri, id) = (s[2], None)
-    else:                          (uri, id) = (None, s[2])
-    signals.append(rec.new_signal(uri, s[1], id=id, rate=rate))
-
+  for n, s in enumerate(args['signals']):
+    signals.append(rec.new_signal(None, s[1], id=s[2], rate=rate))
 
   frames = 0
   count = 0
   data = [ [] for i in xrange(len(signals)) ] # Independent lists
   #      [ [] ]*len(signals)                  # Shared list object...
-
+  rdfxml = [ ]
   for l in sys.stdin:
-    frames += 1
-    d = l.split()
-    for n, s in enumerate(sig_details):
-      data[n].append(float(d[s[0]]))
-    count += 1
-    if count >= BUFFER_SIZE:
-      for n, s in enumerate(signals): s.append(data[n])
-      count = 0
-      data = [ [] for i in xrange(len(signals)) ] # Independent lists
+    if (len(rdfxml) > 0
+     or l.startswith('<?xml ')
+     or l.startswith('<rdf:RDF ')):
+      rdfxml.append(l)
+    elif l.startswith('</rdf:RDF>'):
+      rdfxml.append(l)
+      rec.save_metadata(''.join(rdfxml), rdf.Format.RDFXML)
+      rdfxml = []
+    else:
+      frames += 1
+      d = l.split(args.get('delimiter'))
+      for n, s in enumerate(args['signals']):
+        data[n].append(float(d[s[0]]))
+      count += 1
+      if count >= BUFFER_SIZE:
+        for n, s in enumerate(signals): s.append(data[n])
+        count = 0
+        data = [ [] for i in xrange(len(signals)) ] # Independent lists
+
   if count > 0:
     for n, s in enumerate(signals):
 #      print data[n]
