c345071e0069a9cac7ef6332c7845dfe903a284f
Dave Brooks <dave@bcs.co.nz>
2013-04-28 20:47:07 +1200

    Look for 'label' and 'desc' options.

--- interface/language.py
+++ interface/language.py
@@ -4,15 +4,19 @@ import pyparsing as pp
 _number = pp.Regex(r"\d+(\.\d*)?")
 _number.setParseAction(lambda t:float(t[0]))
 
+_string = (pp.Suppress('"') + pp.Word(pp.printables + ' ', excludeChars='"') + pp.Suppress('"')
+         | pp.Suppress("'") + pp.Word(pp.printables + ' ', excludeChars="'") + pp.Suppress("'"))
+_word   = pp.Word(pp.printables, excludeChars=' ,;<>[]"\'')
+
 _yesno = (pp.CaselessLiteral('yes') ^ pp.CaselessLiteral('y')
          ^ pp.CaselessLiteral('no') ^ pp.CaselessLiteral('n'))
 _yesno.setParseAction(lambda t:(t[0][0] == 'y'))
 
-_path = pp.Word(pp.printables, excludeChars=' ,;<>[]')
+_path = _word
 _uri  = pp.Combine('<' + _path + '>')
 _pipe = pp.Word(pp.alphas + './', bodyChars=pp.printables, excludeChars=' ,;<>[]')
 
-_units = pp.Group(pp.CaselessKeyword('units') + pp.Suppress('=') + (_uri ^ _path))
+_units = pp.Group(pp.CaselessKeyword('units') + pp.Suppress('=') + (_uri ^ _word))
 _rate  = pp.Group(pp.CaselessKeyword('rate') + pp.Suppress('=') + _number)
 _interval = pp.Group(pp.CaselessKeyword('segment') + pp.Suppress('=')
                    + pp.Group(_number + (pp.Literal('-') ^ pp.Literal(':')) + _number))
@@ -21,35 +25,34 @@ _binary   = pp.Group(pp.CaselessKeyword('binary')
 _metadata = pp.Group(pp.CaselessKeyword('metadata')
                    + pp.Optional(pp.Suppress('=') + _yesno, default=True))
 
-_options  = (_rate | _units | _interval | _binary | _metadata) + pp.Suppress(pp.Optional(','))
+_options  = (_rate | _units | _interval | _binary | _metadata)
 
-#_recording = pp.Group(_uri + pp.Optional(_pipe, default='-') + pp.ZeroOrMore(_options))
+_desc = pp.Group(pp.CaselessKeyword('desc') + pp.Suppress('=') + _string)
+_label = pp.Group(pp.CaselessKeyword('label') + pp.Suppress('=') + (_string ^ _word))
 
 
-_signal  = pp.Group(_uri + pp.Optional(_units) + pp.Suppress(pp.Optional(',')))
-_signals = pp.Group(pp.Suppress('[') + pp.ZeroOrMore(_signal) + pp.Suppress(']'))
-
+_output_options = _options + pp.Suppress(pp.Optional(','))
+_output_signal  = pp.Group(_uri + pp.Optional(_units) + pp.Suppress(pp.Optional(',')))
+_output_signals = pp.Group(pp.Suppress('[') + pp.ZeroOrMore(_output_signal) + pp.Suppress(']'))
 _output_statement = pp.Group(pp.CaselessKeyword('stream')
                           + pp.Group(_uri
                                    + pp.Optional(pp.CaselessKeyword('to').suppress() + _pipe,
                                                             default='stdout')
-                                   + pp.ZeroOrMore(_options))
-                          + _signals)
+                                   + pp.ZeroOrMore(_output_options))
+                          + _output_signals)
 
 
-# _string = 
-# _label = pp.Group(pp.CaselessKeyword('label') + pp.Suppress('=') + _string)
-
-#_inopts = (_rate | _units) + pp.Suppress(pp.Optional(','))
-#_input  = pp.Group(_uri + pp.ZeroOrMore(_inopts) + pp.Suppress(pp.Optional(',')))
-#_inputs = pp.Group(pp.Suppress('[') + pp.ZeroOrMore(_input) + pp.Suppress(']'))
-
+_input_options = (_options | _desc | _label) + pp.Suppress(pp.Optional(','))
+_input_sigopts = (_units | _label | _desc) + pp.Suppress(pp.Optional(','))
+_input_signal  = pp.Group(_uri + pp.ZeroOrMore(_input_sigopts))
+_input_signals = pp.Group(pp.Suppress('[') + pp.ZeroOrMore(_input_signal) + pp.Suppress(']'))
 _input_statement = pp.Group(pp.CaselessKeyword('recording')
                           + pp.Group(_uri
                                    + pp.Optional(pp.CaselessKeyword('from').suppress() + _pipe,
                                                             default='stdin')
-                                   + pp.ZeroOrMore(_options))
-                          + _signals)
+                                   + pp.ZeroOrMore(_input_options))
+                          + _input_signals)
+
 
 _statement = (_input_statement | _output_statement) + pp.Suppress(pp.Optional(pp.oneOf('; ,')))
 _grammer   = pp.ZeroOrMore(_statement)
@@ -84,13 +87,17 @@ if __name__ == '__main__':
       <signal/0> units=<http://www.sbpax.org/uome/list.owl#Millivolt>
       <signal/0> units=mV
       ]
-    recording <http://devel.biosignalml.org/testdata/sinewave> from /tmp/pipe rate = 3 [
-      <signal/0>
-      ] ;
+    recording <http://devel.biosignalml.org/testdata/sinewave> from /tmp/pipe
+     rate = 3
+     desc="An example"
+     [
+       <signal/0> label="abc x" desc="hgv hgv j" units=mV
+       ] ;
     recording <http://devel.biosignalml.org/testdata/sinewave2> units=<my/units> rate = 10 [
       <signal/0> units=mV,
       <s3>
       ]"""
+
 
   for r in parse(defn):
     print r[0]
